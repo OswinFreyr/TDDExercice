fd842711e773c33b5205af324a09e74e82cd9b8f
OswinFreyr <pisanotanguy@gmail.com>
2025-12-09 11:35:12 +0100

    {RED 2}: Initialization constructor bis + getQuantity element

--- src/Laboratory.java
+++ src/Laboratory.java
@@ -9,4 +9,8 @@ public class Laboratory {
         }
     }
 
+    public int getQuantity(String element) {
+        return -1;
+    }
+
 }
--- test/LaboratoryTest.java
+++ test/LaboratoryTest.java
@@ -6,8 +6,21 @@ import static org.junit.jupiter.api.Assertions.*;
 public class LaboratoryTest {
     private Laboratory laboratory;
 
-    @Test void initWithEmptySubstanceListThrowsIllegalArgumentException() {
+    @Test
+    void initWithEmptyElementsListThrowsIllegalArgumentException() {
         assertThrows(IllegalArgumentException.class, () -> new Laboratory(new String[]{}));
+    }
+
+    @Test
+    void initWithRegularElementsList() {
+        var lab = new Laboratory(new String[]{
+                "Hydrogen", "Carbon", "Nitrogen", "Oxygen"
+        });
+        assertEquals(0, lab.getQuantity("Hydrogen"));
+        assertEquals(0, lab.getQuantity("Carbon"));
+        assertEquals(0, lab.getQuantity("Nitrogen"));
+        assertEquals(0, lab.getQuantity("Oxygen"));
+        assertThrows(IllegalArgumentException.class, () -> lab.getQuantity("Helium"));
     }
 
 }
