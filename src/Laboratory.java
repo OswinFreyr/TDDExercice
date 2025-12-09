eba4d2f9c15f96be53bd43232295ac496ef0bd34
OswinFreyr <pisanotanguy@gmail.com>
2025-12-09 11:42:50 +0100

    {GREEN 2}: Initialization constructor bis + getQuantity elements

--- src/Laboratory.java
+++ src/Laboratory.java
@@ -1,17 +1,24 @@
 import java.util.List;
-import java.util.Map;
+import java.util.ArrayList;
 
 public class Laboratory {
+
+    private List<String> elementsList = new ArrayList<>();
 
     public Laboratory(String[] elements) {
         if(elements.length == 0) {
             throw new IllegalArgumentException("Elements list cannot be empty");
         }
+        for(var e : elements) {
+            elementsList.add(e);
+        }
     }
 
     public int getQuantity(String element) {
-        
-        return -1;
+        if(!elementsList.contains(element)) {
+            throw new IllegalArgumentException("Element " + element + " not found");
+        }
+        return 0;
     }
 
 }
