1974dfef51d1f188fbdc36d77f8bd8cc69004f70
OswinFreyr <pisanotanguy@gmail.com>
2025-12-22 19:52:15 +0100

    {RED 23}: assert make known product only

--- test/LaboratoryTest.java
+++ test/LaboratoryTest.java
@@ -345,4 +345,24 @@ public class LaboratoryTest {
         assertEquals(2.0, lab.getQuantity("Water"));
     }
 
+    @Test
+    void assertMakeWithUnknownProductThrowsException() {
+        var lab = new Laboratory(
+                new String[]{"Hydrogen", "Carbon", "Oxygen"},
+                new HashMap<String, Map<String, Double>>() {{
+                    put("Earth", new HashMap<String, Double>() {{
+                        put("Water", 2.0);
+                        put("Oxygen", 1.0);
+                    }});
+                    put("Water", new HashMap<String, Double>() {{
+                        put("Oxygen", 1.0);
+                        put("Hydrogen", 2.0);
+                    }});
+                }}
+        );
+        lab.add("Hydrogen", 5.0);
+        lab.add("Oxygen", 2.0);
+        assertThrows(IllegalArgumentException.class, () -> lab.make("Gaz", 1.0));
+    }
+
 }
