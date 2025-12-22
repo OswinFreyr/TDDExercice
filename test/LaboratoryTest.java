15c6c17559cce939336ba76177c37b883704b619
OswinFreyr <pisanotanguy@gmail.com>
2025-12-22 17:55:28 +0100

    {RED 20}: assert use of element to make product

--- test/LaboratoryTest.java
+++ test/LaboratoryTest.java
@@ -280,4 +280,26 @@ public class LaboratoryTest {
         assertEquals(2.0, lab.make("Water", 2.0));
     }
 
+    @Test
+    void assertUseOfElement() {
+        var lab = new Laboratory(
+                new String[]{"Hydrogen", "Carbon", "Oxygen"},
+                new HashMap<String, Map<String, Double>>() {{
+                    put("Earth", new HashMap<String, Double>() {{
+                        put("Water", 2.0);
+                        put("Oxygen", 1.0);
+                    }});
+                    put("Water", new HashMap<String, Double>() {{
+                        put("Oxygen", 1.0);
+                        put("Hydrogen", 2.0);
+                    }});
+                }}
+        );
+        lab.add("Hydrogen", 2.0);
+        lab.add("Oxygen", 2.0);
+        lab.make("Water", 2.0);
+        assertEquals(0.0, lab.getQuantity("Hydrogen"));
+        assertEquals(1.0, lab.getQuantity("Oxygen"));
+    }
+
 }
